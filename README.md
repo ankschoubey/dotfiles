2f874b2ee345b29a1206378cd196baeb2cc52faf
Ankush Choubey <ankushchoubey@outlook.com>
2025-07-20 15:04:35 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Ankush's Dot Files
+
+## Setup
+
+Run followign in terminal
+
+```bash
+git clone https://github.com/ankschoubey/dotfiles.git $HOME/Documents/Github/dotfiles
+cd $HOME/Documents/Github/dotfiles
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-git clone https://github.com/ankschoubey/dotfiles.git $HOME/Documents/Github/dotfiles
-
-cd $HOME/Documents/Github/dotfiles
-
 brew bundle
 stow .
