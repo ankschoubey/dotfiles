0010673bfaf58ff7459d28aa6ca5bb485ccb4d0e
Ankush Choubey <ankushchoubey@outlook.com>
2025-07-24 02:36:58 +0530

    borders

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ ln -s ${pwd}/aerospace/.aerospace.toml ~/.aerospace.toml
 ln -s "$(pwd)/sketchybar" ~/.config/sketchybar
 ln -s "$(pwd)/ghostty" ~/.config/ghostty
 
+ln -s "$(pwd)/jankyborders" ~/.config/borders
+
+
 gh extension install dlvhdr/gh-dash
 
 eval "$(starship init zsh)"
