829036dc247a25dd0fb5cc5726d3ff49e7e0120d
Ankush Choubey <ankushchoubey@outlook.com>
2025-07-23 12:08:08 +0530

    borders

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ stow .
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ln -s ${pwd}/aerospace/.aerospace.toml ~/.aerospace.toml
+
+ln -s "$(pwd)/sketchybar" ~/.config/sketchybar
