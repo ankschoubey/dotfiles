c961a84b31bc723c7278c4dfd4670e6ce7076bac
Ankush Choubey <ankushchoubey@outlook.com>
2025-11-19 23:33:44 +0530

    sketchybar

--- sketchybar/plugins/memswap.sh
+++ sketchybar/plugins/memswap.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-DEFCOLOR="0x44FFFFFF"
-ALERTCOLOR="0xAAFF0000"
-TOTALSWAP="$(sysctl vm.swapusage | awk '{print $4}')"
+DEFCOLOR="0xFFFFFFFF"
+ALERTCOLOR="0xFFFFFFFF"
+TOTALSWAP_MB="$(sysctl vm.swapusage | awk '{print $4}' | sed 's/M//')"
+TOTALSWAP_GB=$(echo "scale=1; $TOTALSWAP_MB / 1024" | bc)
+TOTALSWAP="${TOTALSWAP_GB}G"
 
 clr=""
 if [ "$TOTALSWAP" != "0.00M" ]; then
@@ -11,4 +13,4 @@ else
     clr="$DEFCOLOR"
 fi
 
-sketchybar --set "$NAME" icon.drawing=off label="RAM: $TOTALSWAP"
+sketchybar --set "$NAME" label="$TOTALSWAP" icon.color="$clr" label.color="$clr"
